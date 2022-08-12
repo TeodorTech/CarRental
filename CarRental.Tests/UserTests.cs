3b8f2377311b64d367194b109808568f858938f6
unknown <teodosius77@gmail.com>
2022-08-12 12:06:16 +0300

    Test the CheckAge method

---
+++ CarRental.Tests/UserTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarRental.Tests;
+using CarRental.Domain;
+using CarRental.Domain.Exceptions;
+
+namespace CarRental.Tests
+{
+   public class UserTests
+    {
+        [Fact]
+        public void CheckAge_ShouldNotThrowExceptionIFLegal()
+        {
+            //Assemble
+            User user = new User();
+            string expected = "You can drive <3";
+
+
+            //Act
+            string actual = user.CheckAge(20);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(5)]
+        [InlineData(-10)]
+        [InlineData(100)]
+        public void CheckAge_ShouldThrowExceptionsIfUnderage(int age)
+        {
+            //Assemble
+            User user = new User();
+
+            //Act
+            
+
+            //Assert
+            Assert.Throws<ExceptionAge>(() =>  user.CheckAge(age));
+        }
+    }
+}
